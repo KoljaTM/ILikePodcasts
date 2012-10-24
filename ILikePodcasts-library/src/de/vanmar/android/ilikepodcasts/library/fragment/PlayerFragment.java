a8492d80fcd57aa873ee023d306120de8061b254
Kolja <Kolja@Bluemoon>
2012-10-24 21:27:14 +0200

    added first version layout for player control

---
+++ ILikePodcasts-library/src/de/vanmar/android/ilikepodcasts/library/fragment/PlayerFragment.java
@@ -0,0 +1,10 @@
+package de.vanmar.android.ilikepodcasts.library.fragment;
+
+import android.support.v4.app.Fragment;
+
+import com.googlecode.androidannotations.annotations.EFragment;
+
+@EFragment(resName = "player")
+public class PlayerFragment extends Fragment {
+
+}
